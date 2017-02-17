de89e1ac66d7852e92cba015240005ac79d2cd99
Abe Fetterman <abe@nomiku.com>
2017-02-16 20:26:28 -0800

    Add tests for getDevices

--- test/api.unit.js
+++ test/api.unit.js
@@ -12,12 +12,12 @@ describe('api', function() {
     password:'password'
   };
 
-  afterEach(function() {
-    fetchMock.restore();
-  })
 
   describe('#authenticate', function() {
 
+    afterEach(function() {
+      fetchMock.restore();
+    })
 
     it('should return error without email or password', function(done) {
       var auth = api.authenticate({}).catch( function(err) {
@@ -30,8 +30,7 @@ describe('api', function() {
       fetchMock.post(api.routes.auth, {error:'not found'})
 
       function checkFetch(arg) {
-        var options=fetchMock.lastOptions()
-        var body=JSON.parse(options.body)
+        var body=JSON.parse(fetchMock.lastOptions().body)
         expect(body).to.deep.equal(emailCredentials)
         done()
       }
@@ -53,7 +52,7 @@ describe('api', function() {
         })
     }))
 
-    it('should return expected result', sinon.test(function(done) {
+    it('should return result from fetch', sinon.test(function(done) {
       var fetchResult={
         user_id:2,
         api_token:"hello"
@@ -65,6 +64,73 @@ describe('api', function() {
       fetchMock.post(api.routes.auth, fetchResult)
 
       api.authenticate(emailCredentials)
+        .then((response) => {
+          expect(response).to.deep.equal(expectedResult)
+          done()
+        })
+        .catch((error) => {
+          done(error)
+        })
+    }))
+  })
+
+
+  var authCredentials = {
+    userID:2,
+    apiToken:'password'
+  };
+
+
+  describe('#getDevices', function() {
+
+    afterEach(function() {
+      fetchMock.restore();
+    })
+
+    it('should return error without userID or token', function(done) {
+      var auth = api.getDevices({}).catch( function(err) {
+        expect(err).to.be.an('error');
+        done()
+      })
+    })
+
+    it('should fetch the right endpoint', function(done) {
+      fetchMock.get(api.routes.devices, {error:'not found'})
+
+      function checkFetch(arg) {
+        expect(fetchMock.called())
+        done()
+      }
+      api.getDevices(authCredentials)
+        .then(checkFetch)
+        .catch(checkFetch)
+    })
+
+    it('should reject if fetch returns error', sinon.test(function(done) {
+      fetchMock.get(api.routes.devices, {error:'not found'})
+
+      api.getDevices(authCredentials)
+        .then((response) => {
+          done(new Error('No error thrown'))
+        })
+        .catch((error) => {
+          expect(error).to.be.an('error')
+          done()
+        })
+    }))
+
+    it('should return result from fetch', sinon.test(function(done) {
+      var fetchResult={
+        user_id:2,
+        api_token:"hello"
+      }
+      var expectedResult={
+        userID:fetchResult.user_id,
+        apiToken:fetchResult.api_token
+      }
+      fetchMock.get(api.routes.devices, fetchResult)
+
+      api.getDevices(authCredentials)
         .then((response) => {
           expect(response).to.deep.equal(expectedResult)
           done()
