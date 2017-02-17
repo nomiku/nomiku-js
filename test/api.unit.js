19a4a02a5dc4255d894fc5a5dbae3b0c049a5744
Abe Fetterman <abe@nomiku.com>
2017-02-16 20:17:56 -0800

    use isomorphic fetch and fetch-mock

--- test/api.unit.js
+++ test/api.unit.js
@@ -3,6 +3,7 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 var proxyquire = require('proxyquire')
 var api = require('../lib/api');
+const fetchMock = require('fetch-mock')
 
 describe('api', function() {
 
@@ -11,29 +12,65 @@ describe('api', function() {
     password:'password'
   };
 
+  afterEach(function() {
+    fetchMock.restore();
+  })
+
   describe('#authenticate', function() {
-    before(function() {
-      fetch = sinon.stub();
-      proxyAPI = proxyquire('../lib/api',{'node-fetch':fetch})
-    })
+
 
     it('should return error without email or password', function(done) {
-      var auth = proxyAPI.authenticate({}).catch( function(err) {
+      var auth = api.authenticate({}).catch( function(err) {
         expect(err).to.be.an('error');
         done()
       })
     })
 
-    it('should return error with bad credentials', sinon.test(function(done) {
-      fetch.returns(Promise.resolve({json:() => {return {error:'not found'}}}));
+    it('should supply the right arguments to fetch', function(done) {
+      fetchMock.post(api.routes.auth, {error:'not found'})
 
-      proxyAPI.authenticate(emailCredentials)
-        .then(function(result) {
-          done(new Error('Did not throw error'))
+      function checkFetch(arg) {
+        var options=fetchMock.lastOptions()
+        var body=JSON.parse(options.body)
+        expect(body).to.deep.equal(emailCredentials)
+        done()
+      }
+      api.authenticate(emailCredentials)
+        .then(checkFetch)
+        .catch(checkFetch)
+    })
+
+    it('should reject if fetch returns error', sinon.test(function(done) {
+      fetchMock.post(api.routes.auth, {error:'not found'})
+
+      api.authenticate(emailCredentials)
+        .then((response) => {
+          done(new Error('No error thrown'))
         })
-        .catch( function(err) {
-          expect(err).to.be.an('error')
+        .catch((error) => {
+          expect(error).to.be.an('error')
           done()
+        })
+    }))
+
+    it('should return expected result', sinon.test(function(done) {
+      var fetchResult={
+        user_id:2,
+        api_token:"hello"
+      }
+      var expectedResult={
+        userID:fetchResult.user_id,
+        apiToken:fetchResult.api_token
+      }
+      fetchMock.post(api.routes.auth, fetchResult)
+
+      api.authenticate(emailCredentials)
+        .then((response) => {
+          expect(response).to.deep.equal(expectedResult)
+          done()
+        })
+        .catch((error) => {
+          done(error)
         })
     }))
   })
